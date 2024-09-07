6c9c01b4be8aacbb9ff7d740f823dcf49ff3b358
Daniel Penelva <d4n.andrade@gmail.com>
2024-09-06 21:42:06 -0300

    feat: criando o component signupClient
    O componente foi criado no caminho basic\components
    comando: ng g c signupClient

---
+++ src/app/basic/components/signup-client/signup-client.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-signup-client',
+  templateUrl: './signup-client.component.html',
+  styleUrl: './signup-client.component.scss'
+})
+export class SignupClientComponent {
+
+}
