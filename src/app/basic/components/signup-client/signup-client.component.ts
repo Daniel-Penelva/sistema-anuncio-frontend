fafcfa2ab184349d255398e8c87697041c10e074
Daniel Penelva <d4n.andrade@gmail.com>
2024-09-06 22:20:36 -0300

    feat: add formulario de cadastro de clientes no component signupClient Este componente é responsável pelo formulário de cadastro de clientes na aplicação Angular. Ele utiliza reactive forms para a validação dos campos e interage com um serviço de autenticação para registrar o cliente. Além disso, exibe notificações de sucesso ou erro ao usuário e redireciona após o cadastro.
    A propriedade validateForm!: FormGroup define o formulário reativo (FormGroup) que será usado para controlar e validar os campos do formulário de cadastro.
    
    No construtor injeta as dependências  ormBuilder (usado para construir o grupo de formulários (FormGroup).), AuthService (erviço de autenticação responsável por realizar o registro do cliente através de uma chamada HTTP.), NzNotificationService (serviço do NG-Zorro usado para exibir notificações de sucesso ou erro na tela.) e Router (usado para redirecionar o usuário após o sucesso do cadastro.)
    
    O Método ngOnInit() é chamado quando o componente é inicializado. O "this.validateForm = this.fb.group({ ... })": Cria um formulário com vários campos e validações. E os campos email, name, lastname, password, checkPassword possuem validações de campo obrigatorio e validação de email.
    
    O método submitForm() é chamado quando o formulário é submetido. (1). O "this.authService.registerClient(this.validateForm.value)" envia os dados do formulário para o serviço de autenticação, que faz uma requisição POST para registrar o cliente. O valor do formulário (this.validateForm.value) é passado para o método registerClient do AuthService. (2) Sucesso: Se o cadastro for bem-sucedido, o método executa a função de sucesso (res => {...}), que: Exibe uma notificação de sucesso com o texto "Cadastrado com Sucesso" por 5 segundos (nzDuration: 5000) e redireciona o usuário para a página de login. (3). Erro: Se ocorrer um erro na requisição, a função de erro é chamada (error => {...}), exibindo uma notificação de erro com a mensagem vinda do backend.

--- src/app/basic/components/signup-client/signup-client.component.ts
+++ src/app/basic/components/signup-client/signup-client.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth/auth.service';
+import { NzNotificationService } from 'ng-zorro-antd/notification';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup-client',
@@ -7,4 +11,32 @@ import { Component } from '@angular/core';
 })
 export class SignupClientComponent {
 
+  validateForm!: FormGroup;
+
+  constructor(
+    private fb: FormBuilder, 
+    private authService: AuthService, 
+    private notification: NzNotificationService, 
+    private router: Router){}
+
+    ngOnInit(){
+      this.validateForm = this.fb.group({
+        email: [null, [Validators.email, Validators.required]],
+        name: [null, [Validators.required]],
+        lastname: [null, [Validators.required]],
+        phone: [null],
+        password: [null, [Validators.required]],
+        checkPassword: [null, [Validators.required]],
+      })
+    }
+
+    submitForm(){
+      this.authService.registerClient(this.validateForm.value).subscribe(
+        res => {
+          this.notification.success('SUCCESS', 'Cadastrado com Sucesso', { nzDuration: 5000 });
+          this.router.navigateByUrl('/login');
+      }, error => {
+        this.notification.error('ERROR', `${error.error}`, { nzDuration: 5000 });
+      });
+    }
 }
