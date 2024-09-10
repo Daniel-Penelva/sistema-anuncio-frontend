a3686d5511a49263d261dad3d6f3615c99fbaca6
Daniel Penelva <d4n.andrade@gmail.com>
2024-09-10 19:56:19 -0300

    feat: add processo de cadastro de uma empresa no component signupCompany (.ts)
    Este componente gerencia o formulário de cadastro de empresas, realizando validações e chamando o serviço de autenticação.
    Caso o registro seja bem-sucedido, uma notificação é exibida e o usuário é redirecionado para a página de login.
    Em caso de erro, uma notificação de erro é exibida.
    
    É utilizado o FormBuilder e FormGroup que são usados para criar e gerenciar formulários reativos no Angular. Tb é utilizado o NzNotificationService que é o serviço da biblioteca NG-Zorro usado para exibir notificações de sucesso ou erro. Tb é utilizado o Router que é usado para redirecionar o usuário para outras páginas após o cadastro, neste caso, a página de login.
    A propriedade validateForm é um FormGroup que representa o formulário de cadastro da empresa. Ele será inicializado no método ngOnInit.
    
    No método ngOnInit cria o formulário reativo usando o FormBuilder e definindo os campos e suas validações.
    
    No método submitForm é chamado quando o formulário é enviado. Ele realiza o processo de cadastro. Utiliza a assinatura da resposta (subscribe) para exibição de notificação de sucesso ou error.

--- src/app/basic/components/signup-company/signup-company.component.ts
+++ src/app/basic/components/signup-company/signup-company.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth/auth.service';
+import { NzNotificationService } from 'ng-zorro-antd/notification';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup-company',
@@ -6,5 +10,34 @@ import { Component } from '@angular/core';
   styleUrl: './signup-company.component.scss'
 })
 export class SignupCompanyComponent {
+
+  validateForm!: FormGroup;
+
+  constructor(
+    private fb: FormBuilder, 
+    private authService: AuthService, 
+    private notification: NzNotificationService, 
+    private router: Router){}
+
+    ngOnInit(){
+      this.validateForm = this.fb.group({
+        email: [null, [Validators.email, Validators.required]],
+        name: [null, [Validators.required]],
+        address: [null, [Validators.required]],
+        phone: [null],
+        password: [null, [Validators.required]],
+        checkPassword: [null, [Validators.required]],
+      })
+    }
+
+    submitForm(){
+      this.authService.registerCompany(this.validateForm.value).subscribe(
+        res => {
+          this.notification.success('SUCCESS', 'Cadastrado com Sucesso', { nzDuration: 5000 });
+          this.router.navigateByUrl('/login');
+      }, error => {
+        this.notification.error('ERROR', `${error.error}`, { nzDuration: 5000 });
+      });
+    }
 
 }
