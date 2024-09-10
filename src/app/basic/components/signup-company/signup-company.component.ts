97125142bbc70f1e8202ea607a5873ded3877c01
Daniel Penelva <d4n.andrade@gmail.com>
2024-09-10 19:13:47 -0300

    feat: criando component signupCompany e add import no NgModule no app.module.ts
    O componente foi criado no caminho basic\components
    comando: ng g c signupCompany

---
+++ src/app/basic/components/signup-company/signup-company.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-signup-company',
+  templateUrl: './signup-company.component.html',
+  styleUrl: './signup-company.component.scss'
+})
+export class SignupCompanyComponent {
+
+}
