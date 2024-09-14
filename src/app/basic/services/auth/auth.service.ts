3654a5696b279af42c143ef8852ec52503cd25fd
Daniel Penelva <d4n.andrade@gmail.com>
2024-09-13 22:29:43 -0300

    feat: add o método login no auth.service.ts utilizando Observable, pipe e map
    Este método login é utilizado para enviar uma requisição de autenticação para o back-end e processar a resposta recebida. Ele usa HttpClient do Angular para fazer uma requisição POST e, em seguida, manipula a resposta.
    O método recebe dois parâmetros, username e password, que representam o nome de usuário e a senha fornecidos pelo usuário durante o login.
    O método utiliza o HttpClient para enviar uma requisição HTTP do tipo POST para o endpoint de autenticação. A URL de destino é construída com BASIC_URL + "authenticate", onde BASIC_URL é a base da URL do servidor, e authenticate é o caminho do endpoint de autenticação. O corpo da requisição ({ username, password }) contém o nome de usuário e a senha enviados como um objeto. O "observe: 'response':" especifica que o método espera receber a resposta completa, incluindo cabeçalhos, corpo e status. Sem isso, apenas o corpo da resposta seria retornado.
    No processamento a resposta com pipe e map. O pipe permite aplicar operadores para processar a resposta observável. Aqui, o operador map é usado para transformar a resposta recebida. A resposta da requisição é do tipo HttpResponse, que contém o corpo da resposta, os cabeçalhos, o status e outras informações.
    O método vai extrair o token da resposta, o "res.headers.get(AUTH_HEADER)" obtém o valor do cabeçalho de autorização, que normalmente contém o token JWT no formato Bearer <token>. A variável AUTH_HEADER armazena o nome do cabeçalho de autenticação, no caso, "Authorization". O método substring(7, tokenLength) é usado para remover a palavra "Bearer " da frente do token. O número 7 corresponde ao comprimento da string "Bearer ".
    O método vai exibir o corpo e o token no console e por fim vai retornar a resposta completa, permitindo que outras partes do código tenham acesso ao corpo da resposta, aos cabeçalhos e ao token.
    
    Como funciona:
    O método login envia uma requisição POST com o nome de usuário e senha para o servidor no endpoint /authenticate. Quando o servidor responde, o método processa a resposta para extrair o token JWT do cabeçalho de autorização. Ele também exibe o corpo da resposta e o token no console para fins de depuração. O token extraído pode ser usado posteriormente para autenticar outras requisições enviadas pelo cliente.

--- src/app/basic/services/auth/auth.service.ts
+++ src/app/basic/services/auth/auth.service.ts
@@ -1,8 +1,9 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 
 const BASIC_URL = "http://localhost:8080/";
+export const AUTH_HEADER = 'authorization';
 
 @Injectable({
   providedIn: 'root'
@@ -11,11 +12,26 @@ export class AuthService {
 
   constructor(private http: HttpClient) { }
 
+  // Método para enviar uma requisição de registro de cliente
   registerClient(signupRequestDTO: any): Observable<any>{
     return this.http.post(BASIC_URL + "client/sign-up", signupRequestDTO);
   }
   
+  // Método para enviar uma requisição de registro de empresa
   registerCompany(signupRequestDTO: any): Observable<any>{
     return this.http.post(BASIC_URL + "company/sign-up", signupRequestDTO);
+  }
+
+  // Método para enviar uma requisição de autenticação
+  login(username: string, password: string){
+    return this.http.post(BASIC_URL + "authenticate", { username, password }, { observe: 'response' }).pipe(
+      map((res: HttpResponse<any>) => {
+        console.log(res.body);
+        const tokenLength = res.headers.get(AUTH_HEADER)?.length;
+        const bearerToken = res.headers.get(AUTH_HEADER)?.substring(7, tokenLength);
+        console.log(bearerToken);
+        return res;
+      })
+    );
   }
 }
