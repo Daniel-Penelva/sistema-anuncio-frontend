75c767d8e40c0b74c763572ee869af9afdb124b3
Daniel Penelva <d4n.andrade@gmail.com>
2024-09-16 20:17:03 -0300

    feat: add logica de login e validação no LoginComponent
    Este componente é responsável pela lógica de login e validação do formulário.
    No construtor é declarado: (1) O formulário reativo (FormGroup) que será utilizado para capturar as credenciais de login (nome de usuário e senha). (2) Uma fábrica para criar instâncias de formulários reativos (FormGroup). Facilita a construção do formulário e a aplicação de validações. (3) Um serviço personalizado responsável por enviar as credenciais de login para o servidor, contendo o método login. (4) Um serviço da biblioteca NG-Zorro (NZ) para exibir notificações, como mensagens de erro em caso de falha de login. (5) O serviço de roteamento do Angular, utilizado para navegar entre diferentes páginas após o login.
    
    O método ngOnInit é chamado quando o componente é inicializado. Nele usa o o FormBuilder para criar o grupo de controles do formulário. Aqui, ele cria dois controles de formulário: userName e password, ambos os campos são definidos como obrigatórios através de Validators.required. Os valores iniciais são null, o que significa que os campos estarão vazios quando o formulário for carregado.
    
    O método submitForm envia as credencias, ou seja,  quando o usuário submete o formulário de login. Aqui ocorre a extração de valores utilizando o método get para acessar os valores dos controles username e password a partir do validateForm. O ! após get() é o operador de "non-null assertion", que informa ao TypeScript que o valor não será null. Esse método chama o método login do AuthService, passando as credenciais capturadas. Esse método retorna um Observable, que é assinado (com subscribe) para receber a resposta do servidor. Por fim manipula a notificação de sucesso e erro, se o login for bem-sucedido, a resposta (res) é registrada no console. Se ocorrer um erro durante o login (por exemplo, se as credenciais forem inválidas), é exibida uma notificação de erro utilizando o serviço NzNotificationService.

--- src/app/basic/components/login/login.component.ts
+++ src/app/basic/components/login/login.component.ts
@@ -1,10 +1,45 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { NzNotificationService } from 'ng-zorro-antd/notification';
+import { AuthService } from '../../services/auth/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+  validateForm!: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private notification: NzNotificationService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.validateForm = this.fb.group({
+      userName: [null, [Validators.required]],
+      password: [null, [Validators.required]],
+    })
+  }
+
+  // Método para enviar as crendenciais
+  submitForm(){
+    this.authService.login(this.validateForm.get(['username'])!.value, this.validateForm.get(['password'])!.value).subscribe(
+      res => {
+        console.log(res);
+    }, error => {
+      this.notification.error('ERROR', 'Credenciais inválida', { nzDuration: 5000 });
+    })
+  }
 
 }
+
+/**
+ * Esse componente captura o nome de usuário e a senha inseridos pelo usuário, valida os campos para garantir que ambos sejam preenchidos, e 
+ * envia as credenciais para o servidor usando o AuthService. Se o login for bem-sucedido, o usuário é redirecionado para uma página específica. 
+ * Em caso de falha, uma notificação é exibida informando que as credenciais são inválidas.
+*/
