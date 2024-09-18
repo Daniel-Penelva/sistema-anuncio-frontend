957a6347cb010ec87d2fa62c305fb20d0e2ac0ec
Daniel Penelva <d4n.andrade@gmail.com>
2024-09-17 23:06:10 -0300

    feat: add logica de estado de login e logout do usuário (cliente e empresa) no AppComponent (.ts)
    Define o componente principal. le gerencia o estado de login do usuário (cliente ou empresa) e implementa a funcionalidade de logout.
    
    O atributo isClientLoggedIn é do tipo booleano que armazena se um cliente está logado. Inicialmente, é definido com o valor retornado pelo método estático UserStorageService.isClientLoggedIn(), que verifica se o cliente está autenticado.
    O atributo isCompanyLoggedIn é outro atributo booleano que armazena se uma empresa está logada, obtendo seu valor inicial por meio de UserStorageService.isCompanyLoggedIn().
    
    O método ngOnInit() é executado assim que inicia o componente. Nele se inscreve os eventos de navegação do roteador. Toda vez que uma rota é alterada (por exemplo, quando o usuário navega entre páginas), esse código é executado, o que atualiza o status de login, verificando novamente se o cliente ou a empresa está logado. Assim, o estado de login (isClientLoggedIn e isCompanyLoggedIn) será atualizado automaticamente toda vez que o usuário navega para uma nova rota.
    
    O método logout() é responsável por desconectar o usuário do sistema. O "UserStorageService.signout()" esse método limpa as informações de autenticação armazenadas no localStorage (token e dados do usuário). O "this.router.navigateByUrl('login')" após o logout, o usuário é redirecionado para a página de login, garantindo que o sistema o leve a uma interface apropriada após ser desconectado.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { UserStorageService } from './basic/services/storage/user-storage.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'sistema-anuncio-frontend';
+
+  isClientLoggedIn: boolean = UserStorageService.isClientLoggedIn();
+  isCompanyLoggedIn: boolean = UserStorageService.isCompanyLoggedIn();
+
+  constructor(private router: Router){}
+
+  // Método responsável por se inscrever nos eventos de navegação do roteador. Aqui, o estado de login é atualizado automaticamente sempre que ocorre uma navegação, garantindo que a interface sempre reflita o estado de autenticação do usuário.
+  ngOnInit(): void {
+    this.router.events.subscribe(event => {
+      this.isClientLoggedIn = UserStorageService.isClientLoggedIn();
+      this.isCompanyLoggedIn = UserStorageService.isCompanyLoggedIn();
+    });
+  }
+
+  // Método responsável por desconectar o usuário do sistema.
+  logout(){
+    UserStorageService.signout();
+    this.router.navigateByUrl('login');
+  }
 }
+
+
+/**
+ * Interações com UserStorageService:
+ * 
+ * O UserStorageService é responsável por gerenciar as informações de autenticação no localStorage.
+ * 
+ * Os métodos isClientLoggedIn e isCompanyLoggedIn são usados para determinar o tipo de usuário que está logado, verificando a presença de um 
+ * token e o papel associado (cliente ou empresa).
+ * 
+ * O método signout() remove esses dados do armazenamento local, efetivamente desconectando o usuário.
+*/
