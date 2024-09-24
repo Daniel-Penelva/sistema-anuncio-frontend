5d78fc1cb90b54ea1e5fd18ff1fa90bc1bce415d
Daniel Penelva <d4n.andrade@gmail.com>
2024-09-23 21:43:18 -0300

    feat: criando o método postAd() e createAuthorizationHeader() no company.service
    O serviço companyService é responsável por interagir com a API backend que lida com o sistema de anúncios. O serviço inclui métodos para fazer requisições HTTP relacionadas à criação de anúncios, além de manipular os cabeçalhos de autorização.
    
    A Variável BASIC_URL é uma variável constante que define a URL base da API, que está sendo executada localmente no endereço http://localhost:8080/. Todas as requisições feitas pelo serviço terão essa URL como ponto de partida.
    
    No construtor possui o serviço HttpClient que permite fazer requisições HTTP como GET, POST, PUT, etc o que facilita a comunicação com o backend.
    
    O método postAd() é responsável por fazer uma requisição POST para o backend com o objetivo de criar um anúncio. Recebe como parametro adDTO que é um objeto que contém os dados do anúncio a serem enviados para o backend, representando o corpo da requisição. O userId é recuperado do serviço UserStorageService, que armazena informações do usuário logado no lado do cliente (no caso, no localStorage).  requisição HTTP POST é enviada para a URL, onde {userId} é o ID do usuário que está criando o anúncio. O adDTO é enviado como o corpo da requisição. A requisição inclui cabeçalhos de autorização, que são gerados pelo método createAuthorizationHeader().
    
    O método createAuthorizationHeader() cria e retorna os cabeçalhos de autorização que são usados para autenticar a requisição, no caso, o HttpHeaders é um objeto que vai ser utilizado para definir os cabeçalhos HTTP que serão enviados na requisição. O cabeçalho "Authorization" é configurado com o token de autenticação do usuário. O token é recuperado do UserStorageService e inserido no formato Bearer <token>, que é um padrão comum para autenticação baseada em token (JWT).
    Vale ressaltar que UserStorageService é o serviço que armazena e recupera informações sobre o usuário, como o userId e o token JWT. Ele é usado para garantir que as requisições HTTP sejam feitas em nome de um usuário autenticado.

--- src/app/company/services/company.service.ts
+++ src/app/company/services/company.service.ts
@@ -1,9 +1,27 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { UserStorageService } from '../../basic/services/storage/user-storage.service';
+import { Observable } from 'rxjs';
+
+const BASIC_URL = "http://localhost:8080/";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CompanyService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  // Método responsável por fazer uma requisição POST para o backend com o objetivo de criar um anúncio.
+  postAd(adDTO: any): Observable<any> {
+    const userId = UserStorageService.getUserId();
+    return this.http.post(BASIC_URL + `api/company/ad/${userId}`, adDTO, { headers: this.createAuthorizationHeader() });
+  }
+
+
+  // Método responsável de criar e retornar os cabeçalhos de autorização que são usados para autenticar a requisição HttpHeaders.
+  createAuthorizationHeader(): HttpHeaders {
+    let authHeaders: HttpHeaders = new HttpHeaders();
+    return authHeaders.set('Authorization', 'Bearer ' + UserStorageService.getToken())
+  }
 }
