7f7370251e74990d1f34b8bd3857280c36f03269
Daniel Penelva <d4n.andrade@gmail.com>
2024-09-24 21:06:25 -0300

    feat: add métodos ngOnInit, onFileSelected, previewImage e postAd no componente createAd (.ts)
    Este componente é responsável por criar um formulário para a criação de um anúncio (Ad). Ele gerencia a seleção de arquivos de imagem, a visualização de prévias de imagem e o envio dos dados do anúncio, incluindo o arquivo de imagem, para o backend.
    
    Atributos do componente:
    (1) selectedFile: File | null: Armazena o arquivo de imagem selecionado pelo usuário no formulário.
    (2) imagePreview: string | ArrayBuffer | null: Contém a URL da imagem para ser exibida como pré-visualização após o upload do arquivo.
    (3) validateForm: FormGroup: Este é o formulário reativo que o Angular utiliza para validar e manipular os dados do anúncio.
    
    No construtor seram injetados FormBuild (para criação e a estruturação do formulário reativo), NzNotificationService (serviço da biblioteca NG-Zorro que é usado para exibir notificações visuais de sucesso ou erro.), Router (serviço de roteamento do Angular que permite redirecionar o usuário para diferentes rotas da aplicação.) e CompanyService (serviço injetado que lida com as requisições HTTP para o backend, responsável por criar o anúncio.)
    
    O método ngOnInit representa o formulário reativo onde é construido usando o FormBuilder (fb). O formulário contém três campos: serviceName, description, e price, todos com a validação Validators.required, ou seja, são obrigatórios.
    
    O método onFileSelected é quando o usuário seleciona uma imagem, este método captura o arquivo (via event.target.files[0]) e o armazena na variável selectedFile. Após a seleção, ele chama o método previewImage() para gerar uma pré-visualização da imagem.
    
    O método previewImage utiliza um FileReader para converter o arquivo selecionado em uma URL legível, que pode ser exibida como uma pré-visualização da imagem no navegador. O método reader.onload() é disparado quando o arquivo foi completamente lido, e o resultado é armazenado na variável imagePreview. O readAsDataURL() converte o arquivo em uma URL Base64 que pode ser usada para exibir a imagem no HTML.
    
    O método postAd realiza o envio do formulário e do arquivo de imagem para o backend. Ele cria um objeto FormData, que permite empacotar dados, incluindo arquivos, para serem enviados via requisições HTTP (no caso, um arquivo de imagem). Depois são adicionados campos ao FormData, ou seja, o arquivo de imagem selecionado é adicionado com o nome img e os campos serviceName, description, e price são extraídos do formulário reativo e adicionados ao FormData. Já o método postAd() do CompanyService é chamado, enviando o FormData para o backend. Ele faz uma requisição HTTP do tipo POST. Por fim o tratamento de resposta para o caso de sucesso e erro.

--- src/app/company/pages/create-ad/create-ad.component.ts
+++ src/app/company/pages/create-ad/create-ad.component.ts
@@ -1,10 +1,66 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { NzNotificationService } from 'ng-zorro-antd/notification';
+import { CompanyService } from '../../services/company.service';
 
 @Component({
   selector: 'app-create-ad',
   templateUrl: './create-ad.component.html',
-  styleUrl: './create-ad.component.scss'
+  styleUrl: './create-ad.component.scss',
 })
 export class CreateAdComponent {
+  
+  selectedFile: File | null;
+  imagePreview: string | ArrayBuffer | null;
+  validateForm!: FormGroup;
 
+  constructor(
+    private fb: FormBuilder,
+    private notification: NzNotificationService,
+    private router: Router,
+    private companyService: CompanyService
+  ) {}
+
+  // Método executado qnd o componente é inicializado. Método constroi o formulário reativo.
+  ngOnInit(): void {
+    this.validateForm = this.fb.group({
+      serviceName: [null, [Validators.required]],
+      description: [null, [Validators.required]],
+      price: [null, [Validators.required]],
+    });
+  }
+
+  // Método que seleciona e captura o arquivo de imagem.
+  onFileSelected(event: any) {
+    this.selectedFile = event.target.files[0];
+    this.previewImage();
+  }
+
+  // Método que exibe a imagem
+  previewImage() {
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.imagePreview = reader.result;
+    };
+
+    reader.readAsDataURL(this.selectedFile);
+  }
+
+  // Método que realiza o envio do formulário e do arquivo de imagem.
+  postAd() {
+    const formData: FormData = new FormData();
+
+    formData.append('img', this.selectedFile);
+    formData.append('serviceName', this.validateForm.get('serviceName').value);
+    formData.append('description', this.validateForm.get('description').value);
+    formData.append('price', this.validateForm.get('price').value);
+
+    this.companyService.postAd(formData).subscribe(res => {
+      this.notification.success('SUCCESS', 'Ad Posted Successfully!', { nzDuration: 5000 });
+      this.router.navigateByUrl('/company/ads');
+    }, error => {
+      this.notification.error('ERROR', `${error.error}`, { nzDuration: 5000 });
+    })
+  }
 }
