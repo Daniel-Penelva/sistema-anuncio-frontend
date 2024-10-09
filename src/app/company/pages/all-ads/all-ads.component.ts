9f48f570d26e98e271cda05363aec795b724b121
Daniel Penelva <d4n.andrade@gmail.com>
2024-10-08 21:42:12 -0300

    feat: criando o componente allAds
    Vai ser criado dentro do diretorio ...\app\company\pages o componente allAds
    
    Comando:
    ng g c allAds

---
+++ src/app/company/pages/all-ads/all-ads.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-all-ads',
+  templateUrl: './all-ads.component.html',
+  styleUrl: './all-ads.component.scss'
+})
+export class AllAdsComponent {
+
+}
