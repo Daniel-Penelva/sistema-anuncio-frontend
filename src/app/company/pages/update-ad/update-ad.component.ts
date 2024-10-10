15e8732db288a7db0f598d1194cd314d88b2b66c
Daniel Penelva <d4n.andrade@gmail.com>
2024-10-09 22:33:17 -0300

    feat: (CORREÇÃO) alterando a rota do botão 'alterar' do AllAdsComponent (.html) + add a rota 'update/:id' no company-routing + fornecendo codigo no UpdateAdComponent (.ts)
    Corrigindo o routerLink do botão 'alterar' para routerLink="/company/update/{{ad.id}}".
    
    Definindo a rota no sistema de navegação da aplicação. O "path: update/:id" especifica o caminho da URL que o usuário deve acessar para que essa rota seja ativada. E o "component: UpdateAdComponent" define qual componente Angular será carregado quando essa rota for ativada. O trecho 'update/:id' indica que a rota "update" deve ser seguida de um parâmetro dinâmico chamado id. O : na frente de id define que é um parâmetro de rota. No componente UpdateAdComponent, esse parâmetro pode ser acessado via ActivatedRoute, aqui, "this.activatedRoute.snapshot.params['id']" captura o valor do parâmetro id na URL e armazena na variável adId.
    
    No componente UpdateAdComponent carrega os dados de um anúncio específico com base no seu ID, para possibilitar a atualização desses dados.
    Aqui, "adId: any = this.activatedRoute.snapshot.params['id'];", a propriedade adId é inicializada capturando o parâmetro id da rota atual. O objeto ActivatedRoute permite acessar informações da rota ativa, como os parâmetros passados na URL. Se a rota for algo como /company/update/2, o id será extraído e atribuído à variável adId, no caso o valor será 2. No construtor injeta dois serviços, o "CompanyService" usado para fazer requisições HTTP ao back-end para recuperar os dados do anúncio e o ActivatedRoute usado para acessar os parâmetros da rota atual (nesse caso, o ID do anúncio).
    O método getAdById() usa o CompanyService para fazer uma requisição HTTP GET e buscar os dados do anúncio com base no adId extraído da URL. O método subscribe() é usado para processar a resposta assíncrona. Quando a resposta chega, ela é registrada no console via console.log(res). E o tratamento de erro que apenas registra a resposta no console. É importante adicionar um bloco error no subscribe para lidar com possíveis erros ao buscar o anúncio.

--- src/app/company/pages/update-ad/update-ad.component.ts
+++ src/app/company/pages/update-ad/update-ad.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { CompanyService } from '../../services/company.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-update-ad',
   templateUrl: './update-ad.component.html',
-  styleUrl: './update-ad.component.scss'
+  styleUrl: './update-ad.component.scss',
 })
 export class UpdateAdComponent {
+  
+  adId: any = this.activatedRoute.snapshot.params['id'];
 
+  constructor(
+    private companyService: CompanyService,
+    private activatedRoute: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.getAdById();
+  }
+
+  getAdById() {
+    this.companyService.getAdById(this.adId).subscribe(
+      res => {
+        console.log(res);
+    }, error => {
+      console.error('Erro ao buscar anúncio', error);
+    });
+  }
 }
