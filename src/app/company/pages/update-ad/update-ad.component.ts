7ce997d98031e29c8ce2273a82451a1a1dc60c6a
Daniel Penelva <d4n.andrade@gmail.com>
2024-10-09 21:22:45 -0300

    feat: criando o componente updateAd
    Vai ser criado dentro do diretorio ...\app\company\pages o componente updateAd
    
    Comando:
    ng g c updateAd

---
+++ src/app/company/pages/update-ad/update-ad.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-update-ad',
+  templateUrl: './update-ad.component.html',
+  styleUrl: './update-ad.component.scss'
+})
+export class UpdateAdComponent {
+
+}
