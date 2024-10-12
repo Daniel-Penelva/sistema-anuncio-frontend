6b0ad8dd1218a13ab2bd7eb13455f3b05aa5386a
Daniel Penelva <d4n.andrade@gmail.com>
2024-10-12 00:09:30 -0300

    feat: add implementação para alterar anuncio no UpdateAdComponent (.ts)
    O componente UpdateAdComponent (.ts) é responsável por gerenciar a atualização de um anúncio existente, incluindo a possibilidade de modificar a imagem associada a ele.
    Possui os seguintes atributos e dependencias:
    adId: O ID do anúncio é extraído diretamente da rota ativa (ou seja, a URL) por meio do ActivatedRoute. Esse ID será utilizado para identificar qual anúncio está sendo atualizado.
    selectedFile: Armazena o arquivo de imagem selecionado pelo usuário.
    imagePreview: Mantém uma pré-visualização da imagem para exibi-la ao usuário antes de confirmar a atualização.
    validateForm: Um FormGroup que define o formulário reativo e suas validações.
    existingImage: Armazena a imagem existente no anúncio, caso ela já tenha sido carregada previamente.
    imgChanged: Um booleano que indica se a imagem foi modificada pelo usuário.
    O método ngOnInit inicializa o formulário reativo com os campos obrigatórios e o método getAdById que busca os dados do anúncio com base no "adId" obtido da rota.
    
    O método onFileSelected é chamado quando o usuário seleciona um novo arquivo de imagem para o anúncio. (1) Captura o arquivo de imagem selecionado pelo usuário. (2) Atualiza a pré-visualização da imagem chamando o método previewImage. (3) Seta "existingImage" como null para limpar a imagem antiga e marca imgChanged como true para indicar que a imagem foi alterada.
    
    O método "previewImage" usa o FileReader para carregar o arquivo de imagem selecionado e gerar uma pré-visualização. O resultado da leitura é armazenado em imagePreview, que pode ser uma string base64 ou uma URL.
    
    O método "updateAd" realiza a atualização do anúncio. (1) Cria um objeto FormData, que é utilizado para enviar tanto os dados do formulário quanto o arquivo de imagem (se modificado) para o servidor. (2) Se a imagem foi alterada (imgChanged) e o arquivo selectedFile está presente, ele adiciona o arquivo ao FormData. (3) Adiciona os valores dos campos serviceName, description e price ao FormData. (4) Usa o companyService para enviar uma requisição ao backend chamando o método updateAd. O ID do anúncio e o FormData são passados como parâmetros. Em caso de sucesso: Uma notificação de sucesso é exibida, e o usuário é redirecionado para a página de anúncios da empresa (/company/ads). Em caso de erro: Uma notificação de erro é exibida com a mensagem recebida do backend.
    
    O método "getAdById" faz uma requisição para buscar os dados do anúncio baseado no ID (adId). Quando a resposta é recebida: (1) Os dados do anúncio são inseridos nos campos do formulário usando patchValue, que preenche os controles do formulário com os valores recebidos. (2) A imagem existente do anúncio é convertida para base64 e atribuída a existingImage, para exibir a imagem atual do anúncio.

--- src/app/company/pages/update-ad/update-ad.component.ts
+++ src/app/company/pages/update-ad/update-ad.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { CompanyService } from '../../services/company.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { NzNotificationService } from 'ng-zorro-antd/notification';
 
 @Component({
   selector: 'app-update-ad',
@@ -11,19 +13,82 @@ export class UpdateAdComponent {
   
   adId: any = this.activatedRoute.snapshot.params['id'];
 
+  selectedFile: File | null;
+  imagePreview: string | ArrayBuffer | null;
+  validateForm!: FormGroup;
+  existingImage: string | null = null;
+
+  imgChanged = false;
+
   constructor(
+    private fb: FormBuilder,
+    private notification: NzNotificationService,
+    private router: Router,
     private companyService: CompanyService,
     private activatedRoute: ActivatedRoute
   ) {}
 
+  // Método executado qnd o componente é inicializado. Método constroi o formulário reativo.
   ngOnInit(): void {
+    this.validateForm = this.fb.group({
+      serviceName: [null, [Validators.required]],
+      description: [null, [Validators.required]],
+      price: [null, [Validators.required]],
+    });
+
     this.getAdById();
   }
 
+  // Método que seleciona e captura o arquivo de imagem.
+  onFileSelected(event: any) {
+    this.selectedFile = event.target.files[0];
+    this.previewImage();
+    this.existingImage = null;
+    this.imgChanged = true;
+  }
+
+  // Método que que carrega o arquivo de imagem
+  previewImage() {
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.imagePreview = reader.result;
+    };
+
+    reader.readAsDataURL(this.selectedFile);
+  }
+
+  // Método que realiza a atualização do envio do formulário e do arquivo de imagem.
+  updateAd() {
+    const formData: FormData = new FormData();
+
+    if(this.imgChanged && this.selectedFile){
+      formData.append('img', this.selectedFile);
+    }
+
+    formData.append('serviceName', this.validateForm.get('serviceName').value);
+    formData.append('description', this.validateForm.get('description').value);
+    formData.append('price', this.validateForm.get('price').value);
+
+    // inspecionar o conteudo da imagem e dos campos serviceName, description e price
+    console.log(this.selectedFile);
+    console.log("Imagem: " + formData.get('img'));
+    console.log("Nome do Serviço: " + formData.get('serviceName') + " Descrição: " + formData.get('description') + " Preço: " + formData.get('price'));
+
+    this.companyService.updateAd(this.adId, formData).subscribe(res => {
+      this.notification.success('SUCCESS', 'Anúncio atualizado e publicado com sucesso!', { nzDuration: 5000 });
+      this.router.navigateByUrl('/company/ads');
+    }, error => {
+      this.notification.error('ERROR', `${error.error}`, { nzDuration: 5000 });
+    })
+  }
+
+  // Este método faz uma requisição para buscar os dados do anúncio baseado no ID (adId).
   getAdById() {
     this.companyService.getAdById(this.adId).subscribe(
       res => {
         console.log(res);
+        this.validateForm.patchValue(res);
+        this.existingImage = 'data:image/jpeg;base64,' + res.returnedImg;
     }, error => {
       console.error('Erro ao buscar anúncio', error);
     });
