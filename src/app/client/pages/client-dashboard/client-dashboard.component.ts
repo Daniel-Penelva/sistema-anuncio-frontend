4679c38a5b5469e2d9d1659385c250471c5bea91
Daniel Penelva <d4n.andrade@gmail.com>
2024-10-14 22:28:35 -0300

    feat: add os métodos getAllAds e updateImage no ClientDashboardComponent (.ts)
    Este componente é responsável por buscar e exibir uma lista de anúncios (ads) e transformar imagens codificadas em Base64 para exibição no HTML.
    
    A propriedade "ads" é declarada e inicializada como um array vazio. Esse array armazenará a lista de anúncios que serão buscados do backend.
    
    No construtor o ClientService é injetado, o que permite que o componente faça chamadas ao backend para obter os anúncios.
    
    O método ngOnInit() é inicializado assim que o  componente é executado, dentro deste método, o componente chama o método getAllAds() para buscar os anúncios assim que o componente é carregado.
    
    O método getAllAds() busca os anúncios, ele faz uma chamada ao ClientService (onde está a lógica de requisição HTTP) para buscar todos os anúncios. O método getAllAds() do ClientService retorna um Observable, que o componente se inscreve através de .subscribe(). Quando a resposta da requisição é recebida com sucesso, o callback da função anônima é chamado, onde, a propriedade "ads" do componente é atualizada com o resultado da requisição (res), que é a lista de anúncios recebida do backend.
    
    O método updateImg() coverte imagens Base64, ele é utilizado para transformar imagens codificadas em Base64 (formato de texto que representa dados binários, como imagens) em uma URL utilizável no HTML. A função retorna uma URL de dados que pode ser utilizada diretamente em tags de imagem no HTML, sem precisar de um caminho de arquivo físico. Como funciona? O método recebe a string de uma imagem codificada em Base64 como parâmetro (img). Ele então retorna a string no formato esperado pelo navegador para exibir uma imagem, que é: data:image/jpeg;base64,<imagem_codificada>. Esse formato de URL permite exibir a imagem diretamente no HTML.

--- src/app/client/pages/client-dashboard/client-dashboard.component.ts
+++ src/app/client/pages/client-dashboard/client-dashboard.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { ClientService } from '../../services/client.service';
 
 @Component({
   selector: 'app-client-dashboard',
   templateUrl: './client-dashboard.component.html',
-  styleUrl: './client-dashboard.component.scss'
+  styleUrl: './client-dashboard.component.scss',
 })
 export class ClientDashboardComponent {
+  ads: any = [];
+
+  constructor(private clientService: ClientService) {}
+
+  ngOnInit(): void {
+    this.getAllAds();
+  }
+
+  // Método para buscar todos os anúncios (Ads)
+  getAllAds() {
+    this.clientService.getAllAds().subscribe((res) => {
+      this.ads = res;
+    });
+  }
+
+  // O método updateImg() é usada para transformar a imagem codificada em Base64 em uma URL utilizável no HTML, permitindo a exibição de imagens diretamente sem a necessidade de um caminho de arquivo físico.
+  updateImg(img) {
+    return 'data:image/jpeg;base64,' + img;
+  }
 
 }
