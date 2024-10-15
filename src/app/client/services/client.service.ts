5171ac7b772e1b00311ce88f26b2b5b177508d2f
Daniel Penelva <d4n.andrade@gmail.com>
2024-10-14 22:10:11 -0300

    feat: add os métodos getAllAds e createAuthorizationHeader no client.service.ts utilizando Observable
    Este service vai ser responsável por fazer requisições HTTP ao backend da aplicação para lidar com anúncios (ads).
    
    Foi declarado a URL base que  define a URL base da API do backend, que está sendo executada localmente no endereço http://localhost:8080/.
    
    No serviço client utiliza o HttpClient, que é injetado no construtor, para fazer requisições HTTP ao backend. O HttpClient é um módulo padrão do Angular que facilita a comunicação HTTP, sendo muito útil para fazer chamadas RESTful e lidar com as respostas de forma reativa.
    
    O método "getAllAds()" é responsável por buscar todos os anúncios (ads) no backend. Ele faz uma requisição HTTP do tipo GET para o endpoint /api/client/ads, que foi definido no backend. Ele retorna um Observable, que é a maneira reativa do Angular de lidar com fluxos de dados. O Observable permite que você se inscreva (subscribe) e receba a resposta da requisição quando o backend retornar os dados. O método usa o cabeçalho de autorização gerado pelo método createAuthorizationHeader() para enviar o token JWT no cabeçalho da requisição. Esse token é necessário para garantir que a chamada seja autenticada.
    
    O método "createAuthorizationHeader()" cria os cabeçalhos de autorização necessários para autenticar a requisição HTTP. Especificamente, ele adiciona um token JWT (JSON Web Token) ao cabeçalho Authorization. Esse token é obtido do UserStorageService.getToken(). O token é obtido a partir do UserStorageService.getToken(), que é um serviço responsável por armazenar e recuperar o token JWT do local storage ou sessão do navegador. Lembrando que o  JWT é um padrão usado para autenticar solicitações HTTP, geralmente enviado no formato Bearer <token>. O token é utilizado para validar que o usuário tem permissão para acessar os recursos solicitados.

--- src/app/client/services/client.service.ts
+++ src/app/client/services/client.service.ts
@@ -1,9 +1,25 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { UserStorageService } from '../../basic/services/storage/user-storage.service';
+
+const BASIC_URL = "http://localhost:8080/";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClientService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  // Método responsável por buscar todos os anúncios (ads).
+  getAllAds(): Observable<any> {
+    return this.http.get(BASIC_URL + `api/client/ads`, { headers: this.createAuthorizationHeader() });
+  }
+
+  // Método responsável de criar e retornar os cabeçalhos de autorização que são usados para autenticar a requisição HttpHeaders.
+  createAuthorizationHeader(): HttpHeaders {
+    let authHeaders: HttpHeaders = new HttpHeaders();
+    return authHeaders.set('Authorization', 'Bearer ' + UserStorageService.getToken())
+  }
 }
