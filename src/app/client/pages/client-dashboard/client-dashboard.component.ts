204b7fdef81fd328185a9c9c0c9c1cf092f29f18
Daniel Penelva <d4n.andrade@gmail.com>
2024-10-15 23:25:02 -0300

    feat: add o método searchAdByName para campo de pesquisa no clientDasboardComponent (.ts) e add o formulário de pesquisa no clientDasboardComponent (.html)  e customização SCSS
    Foi add a propriedade validateForm que é do tipo FormBuilder para criar um formulário reativo com um campo chamado 'service'. Esse campo é obrigatório, conforme o Validators.required. O formulário é inicializado no ngOnInit(), e o campo 'service' é configurado para ser validado antes de permitir a busca.
    
    No ClientDasboardComponent (.ts)
    O método searchAdByName , o valor do campo 'service' é extraído usando this.validateForm.get(['service']).value e passado para o método searchAdByName do ClientService, que faz uma requisição HTTP ao back-end.
    O método possui uma verificação da lista de anúncios, após receber a resposta do servidor, é verificado se o array this.ads está vazio (sem anúncios). Se estiver, o this.notification.info informa ao usuário que não há anúncios com o nome pesquisado.
    O método possui verifcação do error.status para identificar se o código de status HTTP é 404, o que significa que o recurso não foi encontrado. Se o erro for de outro tipo (exemplo: falha na conexão, erro de servidor), uma mensagem genérica é exibida.
    
    No ClientDasboardComponent (.html)
    Foi add um formulário de pesquisa para buscar anúncios com base no nome do serviço utilizando a biblioteca NG-Zorro, com o uso de um formulário reativo.
    O [formGroup]="validateForm" indica que o formulário está vinculado ao FormGroup reativo chamado validateForm, que é definido no componente TypeScript.
    O (submit)="searchAdByName()" atribui um manipulador de evento para o evento de submissão do formulário. Ao submeter o formulário, o método searchAdByName() será chamado para executar a pesquisa.
    O formControlName="service" vincula o input ao FormControl chamado service dentro do formulário reativo (validateForm). Esse controle é responsável por lidar com o valor, estado e validação do input.
    O [disabled]="validateForm.invalid" desabilita o botão de pesquisa enquanto o formulário for inválido. Como o campo 'service' é obrigatório, o botão só será habilitado quando o campo for preenchido corretamente. O uso do [disabled]="validateForm.invalid" ajuda a evitar o envio do formulário com campos inválidos, promovendo a validação no front-end antes do envio da requisição.
    A mensagem "O campo de busca é obrigatório" será exibida somente se o campo estiver inválido (invalid) e tiver sido tocado (touched). Se o campo não estiver preenchido ou for inválido, essa mensagem será mostrada.
    A mensagem "Nenhum anúncio encontrado com o nome pesquisado" será exibida somente se o campo for válido (valid), ou seja, se o campo foi preenchido corretamente, e nenhum anúncio foi encontrado (ads.length === 0). Isso garante que a mensagem de "Nenhum anúncio encontrado" não seja exibida quando o campo estiver vazio.
    
    A ideia é que se o campo de busca estiver vazio, a mensagem de campo obrigatório será mostrada. Caso contrário, se não houver anúncios e o campo foi tocado, a mensagem "Nenhum anúncio encontrado" será exibida.

--- src/app/client/pages/client-dashboard/client-dashboard.component.ts
+++ src/app/client/pages/client-dashboard/client-dashboard.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { ClientService } from '../../services/client.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NzNotificationService } from 'ng-zorro-antd/notification';
 
 @Component({
   selector: 'app-client-dashboard',
@@ -8,10 +10,17 @@ import { ClientService } from '../../services/client.service';
 })
 export class ClientDashboardComponent {
   ads: any = [];
+  validateForm!: FormGroup;
+  
 
-  constructor(private clientService: ClientService) {}
+  constructor(private clientService: ClientService, 
+    private fb: FormBuilder, 
+    private notification: NzNotificationService) {}
 
   ngOnInit(): void {
+    this.validateForm = this.fb.group({
+      service: [null, [Validators.required]]
+    })
     this.getAllAds();
   }
 
@@ -25,6 +34,24 @@ export class ClientDashboardComponent {
   // O método updateImg() é usada para transformar a imagem codificada em Base64 em uma URL utilizável no HTML, permitindo a exibição de imagens diretamente sem a necessidade de um caminho de arquivo físico.
   updateImg(img) {
     return 'data:image/jpeg;base64,' + img;
+  }
+
+  searchAdByName() {
+    this.clientService.searchAdByName(this.validateForm.get(['service']).value).subscribe(res => {
+      this.ads = res;
+
+      // Verifica se a resposta está vazia
+      if (this.ads.length === 0) {
+        this.notification.info('INFO', 'Nenhum anúncio encontrado com esse nome.', { nzDuration: 5000 });
+      }
+    }, error => {
+      if (error.status === 404) {
+        this.notification.error('ERROR', 'Nenhum anúncio encontrado com esse nome.', { nzDuration: 5000 });
+      } else {
+        this.notification.error('ERROR', 'Ocorreu um erro ao buscar o anúncio. Tente novamente mais tarde.', { nzDuration: 5000 });
+      }
+      console.log("Erro ao buscar anúncio", error);
+    });
   }
 
 }
