2a7b255e61e52b9ad8d5bbb9cd696060f21da449
Daniel Penelva <d4n.andrade@gmail.com>
2024-10-22 22:17:20 -0300

    feat: add implementação no AdDetailComponent (.ts) para testar a busca do anuncio
    Esse componente tem a responsabilidade de buscar e exibir os detalhes de um anúncio específico, com base no parâmetro adId que é passado pela rota.
    
    Foi declarado a propriedade adId que é inicializada com o valor do parâmetro da rota, adId. O parâmetro é extraído da URL usando o objeto ActivatedRoute que fornece informações sobre a rota ativa. A propriedade params['adId'] captura o parâmetro dinâmico da URL que foi definido na configuração de rotas, por exemplo: ad/:adId. O snapshot é usado para capturar o estado atual da rota no momento em que o componente é criado.
    
    No construtor possui o serviço ClientService que é injetado via Dependency Injection. Esse serviço tem a função de lidar com a comunicação HTTP com o backend para obter os detalhes do anúncio e o ActivatedRoute que também é injetado para permitir o acesso aos parâmetros da rota (como adId), além de outras informações úteis sobre a rota ativa.
    
    O método getAdDetailsByAdId(), aqui, this.clientService.getAdDetailsByAdId(this.adId) o this.clientService.getAdDetailsByAdId(this.adId). O método getAdDetailsByAdId() retorna um Observable, que é a forma como o Angular lida com requisições assíncronas (HTTP). O subscribe() é usado para "se inscrever" no Observable e executar a função de callback (nesse caso, o console.log(res)) quando a resposta for recebida. E o tratamento de erros no subscribe() para lidar com cenários onde a requisição falha.

--- src/app/client/pages/ad-detail/ad-detail.component.ts
+++ src/app/client/pages/ad-detail/ad-detail.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { ClientService } from '../../services/client.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-ad-detail',
   templateUrl: './ad-detail.component.html',
-  styleUrl: './ad-detail.component.scss'
+  styleUrl: './ad-detail.component.scss',
 })
 export class AdDetailComponent {
+  
+  adId = this.activatedRoute.snapshot.params['adId'];
 
+  constructor(
+    private clientService: ClientService,
+    private activatedRoute: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.getAdDetailsByAdId();
+  }
+
+  getAdDetailsByAdId() {
+    this.clientService.getAdDetailsByAdId(this.adId).subscribe((res) => {
+      console.log(res);
+    }, error => {
+      console.log('Erro ao buscar os detalhes do anúncio:', error);
+    });
+  }
 }
