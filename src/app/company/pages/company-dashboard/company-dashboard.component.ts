5f2a6c4159cd88729ea432e8c35046aa12fc5019
Daniel Penelva <d4n.andrade@gmail.com>
2024-10-25 21:05:04 -0300

    feat: add o método getAllAdBookings no CompanyDashboardComponent (.ts)
    Este componente CompanyDashboardComponent é responsável por exibir as reservas (ou "bookings") associadas a uma empresa específica no dashboard da empresa.
    
    A propriedade "bookings" armazena a lista de reservas que será exibida no dashboard da empresa. Inicialmente, seu valor é indefinido até que a chamada ao backend retorne os dados.
    
    O construtor injeta o serviço CompanyService, que é utilizado para fazer a requisição HTTP para buscar as reservas da empresa.
    
    O método getAllAdBookings() faz uma requisição assíncrona para o backend, chamando o método getAllAdBookings() do CompanyService. A resposta (res) é registrada no console para fins de depuração e atribuída à propriedade bookings, para que possa ser exibida no componente.
    
    O método ngOnInit é chamado uma vez logo após a inicialização. Aqui, ele chama o método getAllAdBookings() para carregar as reservas no momento em que o componente é carregado.

--- src/app/company/pages/company-dashboard/company-dashboard.component.ts
+++ src/app/company/pages/company-dashboard/company-dashboard.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { CompanyService } from '../../services/company.service';
 
 @Component({
   selector: 'app-company-dashboard',
   templateUrl: './company-dashboard.component.html',
-  styleUrl: './company-dashboard.component.scss'
+  styleUrl: './company-dashboard.component.scss',
 })
 export class CompanyDashboardComponent {
 
+  bookings: any;
+  
+  constructor(private companyService: CompanyService) {}
+
+  ngOnInit(): void {
+    this.getAllAdBookings();
+  }
+
+  getAllAdBookings() {
+    this.companyService.getAllAdBookings().subscribe((res) => {
+      console.log(res);
+      this.bookings = res;
+    }, (error) => {
+      console.error("Erro ao carregar as reservas, ", error);
+    });
+  }
 }
