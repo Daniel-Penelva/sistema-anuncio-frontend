760ffb1332218c1eb0960fc46ce187042889c0c2
Daniel Penelva <d4n.andrade@gmail.com>
2024-10-29 20:20:08 -0300

    feat: add método getMyBookings no MyBookingsComponent (.ts) e add codígo html no MyBookingsComponent (.html)
    No componente MyBookings (.ts) o método getMyBookings é responsável por exibir as reservas feitas pelo usuário logado no componente MyBookingsComponent de um sistema de agendamento ou serviços.
    A propriedade bookedServices que armazena a lista de reservas retornada pelo serviço, que é usada para exibir dados na tabela do template HTML.
    No cosntrutor injeta o serviço ClientService, permitindo o uso do método getMyBookings() para buscar as reservas do usuário logado.
    O método getMyBookings() faz uma requisição ao serviço ClientService para obter as reservas do usuário. Para sucesso armazena a resposta (res) em bookedServices e para erro exibe um log no console com uma mensagem de erro.
    
    No componente MyBookingsComponent (.html) é utilizado uma tabela nz-table com a biblioteca NG-Zorro para exibir os dados da reserva. O [nzData]="bookedServices" é uma fonte de dados da tabela, alimentada pela propriedade bookedServices que contém as reservas. A tabela está configurada para mostrar até 20 itens por página, sem paginação ([nzShowPagination]="false").
    No cabeçalhos da tabela exibe as colunas de informações como "Serviço", "Data", "Status" e "Ação".
    Nas linhas da tabela exibe detalhes específicos da reserva (serviceName, bookDate e reservationStatus). A formatação da Data bookDate é formatado para dd/MM/yyyy, tornando a exibição mais legível.
    Por fim, um botão bookDate é formatado para dd/MM/yyyy, tornando a exibição mais legível.

--- src/app/client/pages/my-bookings/my-bookings.component.ts
+++ src/app/client/pages/my-bookings/my-bookings.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { ClientService } from '../../services/client.service';
 
 @Component({
   selector: 'app-my-bookings',
   templateUrl: './my-bookings.component.html',
-  styleUrl: './my-bookings.component.scss'
+  styleUrl: './my-bookings.component.scss',
 })
 export class MyBookingsComponent {
+  
+  bookedServices: any;
 
+  constructor(private clientService: ClientService) {}
+
+  ngOnInit(): void {
+    this.getMyBookings();
+  }
+
+  // Método responsável por exibir as reservas feitas pelo cliente
+  getMyBookings() {
+    this.clientService.getMyBookings().subscribe(
+      (res) => {
+        this.bookedServices = res;
+      },(error) => {
+        console.log('Erro ao buscar os detalhes do anúncio:', error);
+      });
+  }
 }
