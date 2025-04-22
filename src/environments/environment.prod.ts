cf3fadd4f66a2b968d993d1116150cf1ee3049ce
Daniel Penelva <d4n.andrade@gmail.com>
2025-04-21 21:27:27 -0300

    feat: (funcionalidade: Deploy - parte 1):  criando uma pasta envirnoments e os arquivos environment.ts e environment.prod.ts
    Estes arquivos são utilizados para definir variáveis de ambiente para o ambiente de produção ou para o ambiente de desenvolvimento.

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: true,
+    apiUrl: 'https://backend.anunciefacil.com/'
+};
+// Este arquivo é utilizado para definir variáveis de ambiente para o ambiente de produção.
+// O valor de `production` é definido como `true`, indicando que está em produção.
