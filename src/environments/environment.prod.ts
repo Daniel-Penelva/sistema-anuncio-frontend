0e47f60f5f07753a4285ddd35af68a355f498c91
Daniel Penelva <d4n.andrade@gmail.com>
2025-04-22 01:28:07 -0300

    feat: (funcionalidade: deploy) correção add nova url no environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://backend.anunciefacil.com/'
+    apiUrl: 'https://sistema-anuncio-backend-production.up.railway.app/'
 };
 // Este arquivo é utilizado para definir variáveis de ambiente para o ambiente de produção.
 // O valor de `production` é definido como `true`, indicando que está em produção.
